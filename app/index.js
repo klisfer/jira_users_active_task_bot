4aaacc7e087a02929997fbcfc9e34cfa8ed197a5
aayush chaubey <aayush.chaubey674@gmail.com>
2019-04-09 17:46:04 +0530

    updated response format

--- app/index.js
+++ app/index.js
@@ -1,21 +1,25 @@
 const dotenv = require("dotenv");
-const Bot = (require("@dlghq/dialog-bot-sdk"));
-const { MessageAttachment, ActionGroup, Action, Button } = (require("@dlghq/dialog-bot-sdk"));
-const { flatMap } = require('rxjs/operators');
-const axios = require('axios');
-const { merge } = require('rxjs');
-const moment = require('moment');
-
+const Bot = require("@dlghq/dialog-bot-sdk");
+const {
+  MessageAttachment,
+  ActionGroup,
+  Action,
+  Button
+} = require("@dlghq/dialog-bot-sdk");
+const { flatMap } = require("rxjs/operators");
+const axios = require("axios");
+const { merge } = require("rxjs");
+const moment = require("moment");
 
 dotenv.config();
 
-
-const credentials = process.env.JIRA_USERNAME + ":" + process.env.JIRA_API_TOKEN
-const credsBase64 = Buffer.from(credentials).toString('base64');
+const credentials =
+  process.env.JIRA_USERNAME + ":" + process.env.JIRA_API_TOKEN;
+const credsBase64 = Buffer.from(credentials).toString("base64");
 var inprogressIssues = "";
 const headers = {
-  'Authorization': 'Basic ' + credsBase64,
-  'Content-Type': 'application/json',
+  Authorization: "Basic " + credsBase64,
+  "Content-Type": "application/json"
 };
 var sortedTasks = {};
 
@@ -25,11 +29,9 @@ async function run(token, endpoint) {
     endpoints: [endpoint]
   });
 
-  //fetching bot name  
+  //fetching bot name
   const self = await bot.getSelf();
   console.log(`I've started, post me something @${self.nick}`);
-
-
 
   bot.updateSubject.subscribe({
     next(update) {
@@ -37,108 +39,98 @@ async function run(token, endpoint) {
     }
   });
 
-
-  //subscribing to incoming messages 
-  const messagesHandle = bot
-    .subscribeToMessages()
-    .pipe(flatMap(async (message) => {
+  //subscribing to incoming messages
+  const messagesHandle = bot.subscribeToMessages().pipe(
+    flatMap(async message => {
       console.log("MESSAGE", message);
-      if ((message.content.type === 'text') && (message.content.text === process.env.TEXT_MESSAGE)) {
-
+      if (
+        message.content.type === "text" &&
+        message.content.text === process.env.TEXT_MESSAGE
+      ) {
         let result = await axios({
           url: process.env.JIRA_URL,
-          method: 'get',
-          headers: headers,
-
+          method: "get",
+          headers: headers
         })
           .then(response => {
-            response.data.issues.map((issue) => {
+            response.data.issues.map(issue => {
               // console.log("fetched issues" , issue);
               const creator = issue.fields.creator.displayName;
-              
-              if(sortedTasks.hasOwnProperty(creator.toString())){
-                 sortedTasks[creator.toString()].push(formatJiraText(issue)); 
-              }else{
+
+              if (sortedTasks.hasOwnProperty(creator.toString())) {
+                sortedTasks[creator.toString()].push(formatJiraText(issue));
+              } else {
                 sortedTasks[creator.toString()] = [];
-                sortedTasks[creator.toString()].push(formatJiraText(issue)); 
+                sortedTasks[creator.toString()].push(formatJiraText(issue));
               }
-             
-              
-            })
+            });
             inprogressIssues = sortTasks(sortedTasks);
-            console.log("inprogress" , inprogressIssues);
-            sendTextToBot(bot , message);
+            console.log("inprogress", inprogressIssues);
+            sendTextToBot(bot, message);
             inprogressIssues = "";
-            
-          }).catch(err => {
+          })
+          .catch(err => {
             console.log(err);
           });
       }
-      
-    
-    }));
-
+    })
+  );
 
   //creating action handle
   const actionsHandle = bot
     .subscribeToActions()
-    .pipe(flatMap(async (event) => console.log(JSON.stringify(event, null, 2))));
+    .pipe(flatMap(async event => console.log(JSON.stringify(event, null, 2))));
 
-
-// merging actionHandle with messageHandle
+  // merging actionHandle with messageHandle
   await new Promise((resolve, reject) => {
-    merge(messagesHandle, actionsHandle)
-      .subscribe({
-        error: reject,
-        complete: resolve
-      });
+    merge(messagesHandle, actionsHandle).subscribe({
+      error: reject,
+      complete: resolve
+    });
   });
 }
 
 //token to connect to the bot
 const token = process.env.BOT_TOKEN;
-if (typeof token !== 'string') {
-  throw new Error('BOT_TOKEN env variable not configured');
+if (typeof token !== "string") {
+  throw new Error("BOT_TOKEN env variable not configured");
 }
 
 //bot endpoint
-const endpoint = process.env.BOT_ENDPOINT || 'https://grpc-test.transmit.im:9443';
+const endpoint =
+  process.env.BOT_ENDPOINT || "https://grpc-test.transmit.im:9443";
 
-run(token, endpoint)
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
-
-
+run(token, endpoint).catch(error => {
+  console.error(error);
+  process.exit(1);
+});
 
 function formatJiraText(issue) {
   const timeInProgress = moment(issue.fields.updated).fromNow();
-  const projectId = issue.fields.project.key;
-  const taskId = issue.id;
+  const taskId = issue.key;
   const taskUrl = issue.self;
   const taskTitle = issue.fields.summary;
-  const outputFormat = timeInProgress + " - " + "[" + projectId + "-" + taskId + "](" + taskUrl + ") :" + taskTitle
+  const outputFormat =
+    timeInProgress + " - " + "[" + taskId + "](" + taskUrl + ") :" + taskTitle;
   return outputFormat;
 }
 
 function sendTextToBot(bot, message) {
-  bot
-  .sendText(
+  bot.sendText(
     message.peer,
     inprogressIssues,
-    MessageAttachment.reply(message.id),
+    MessageAttachment.reply(message.id)
   );
 }
 
-function sortTasks(sortedTasks){
+function sortTasks(sortedTasks) {
   var jiraResponse = "";
   const users = Object.keys(sortedTasks);
   users.map(function(key, index) {
-    jiraResponse += "\n"+"@"+key+"\n";
-    sortedTasks[key].map( task => {
+    jiraResponse += "\n" + "@" + key + "\n";
+    sortedTasks[key].map(task => {
       jiraResponse += task + "\n";
-    })
+    });
     // console.log("#" , jiraResponse);
   });
 
